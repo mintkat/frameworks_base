68dbcea0ade3f1e3ebb2604d7de9162ed62a2657
blk_jack <jcorcoran+github@gmail.com>
2015-01-28 23:38:40 -0800

    Base: MediaScanner behavior on boot (1/3)
    Change-Id: Ic237f1eaf0b1452fb09f06304d029d6cac29be22

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4202,6 +4202,15 @@ public final class Settings {
         public static final String RECENTS_USE_OMNISWITCH = "recents_use_omniswitch";
 
         /**
+         * MediaScanner behavior on boot.
+         * 0 = enabled
+         * 1 = ask (notification)
+         * 2 = disabled
+         * @hide
+         */
+         public static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
