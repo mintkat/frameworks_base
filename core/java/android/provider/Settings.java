5a35d7dec51f7cc7a504a96f5453a439919d29be
cristianomatos <cristianobmatos@gmail.com>
2015-02-16 19:18:42 -0200

    [Base] Volume panel timeout (1/2)
    Change-Id: Id2f602cd24c873b82a0a34ee9c8bd78ee373f72d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4317,6 +4317,12 @@ public final class Settings {
         public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
 
         /**
+         * Timeout for volume panel
+         * @hide
+         */
+        public static final String VOLUME_PANEL_TIMEOUT = "volume_panel_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
