9c1b0f7d67a82be6cc1720d0100289de5c5a413c
westcripp <korukaltan@gmail.com>
2015-06-03 03:45:26 +0300

    Remove the empty notification after switch on/off logic
    Change-Id: I3a5a8716208140d3d4e978a2eb782af5ec29c90f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -513,7 +513,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                             mContext.getContentResolver(),
                             Settings.System.ENABLE_TASK_MANAGER,
                             0, UserHandle.USER_CURRENT) == 1;
-                             Helpers.restartSystemUI();
+                            Helpers.restartSystemUI();
+                            updateRowStates();
+                            updateSpeedbump();
+                            updateClearAll();
+                            updateEmptyShadeView();
             }
 
             update();
