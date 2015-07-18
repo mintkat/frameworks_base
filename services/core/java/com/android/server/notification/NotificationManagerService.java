47860702cb92a8b4b07aa71aa6effcc57d57c9f1
AdrianDC <radian.dc@gmail.com>
2015-07-18 12:20:51 +0200

    Screen-On Lights [1/2]: Optional global allowment of lights
    Implement a setting to allow lights to be activated
    for new notifications even if the screen is on.
    
    Always allow and Custom values are separated
    in an advanced section for a cleaner overview.
    
    This setting gives the user an oportunity to activate lights
    with the screen on and also during DayDream screensaver,
    which many users requested especially for the Xperia SP.
    
    The option is not activated by default for every phone.
    
    Changes include :
      frameworks/base
      packages/Apps/Settings
    
    Screenshot of the Settings :
      http://i1285.photobucket.com/albums/a583/adriandc/Screenshot_2015-07-20-01-01-20_zpssynaf1la.png~original
    
    Change-Id: I2071147d8ddab80ba0e1e7310e785ac3e03b3c7c
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -217,6 +217,9 @@ public class NotificationManagerService extends SystemService {
     private int mDefaultNotificationLedOff;
     private long[] mDefaultVibrationPattern;
 
+    private boolean mAlwaysAllowedEnabledSetting = false;
+    private boolean mAlwaysAllowedEnabledDefault = false;
+
     private long[] mFallbackVibrationPattern;
     private boolean mUseAttentionLight;
     boolean mSystemReady;
@@ -870,6 +873,9 @@ public class NotificationManagerService extends SystemService {
             resolver.registerContentObserver(
                     ENABLED_NOTIFICATION_LISTENERS_URI, false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NOTIFICATION_LIGHT_ALWAYS_ALLOWED),
+                    false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR),
                     false, this, UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
@@ -933,6 +939,11 @@ public class NotificationManagerService extends SystemService {
                        Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, 0,
                        UserHandle.USER_CURRENT_OR_SELF);
             }
+
+            // Notification always allowed
+            mAlwaysAllowedEnabledSetting = (Settings.System.getIntForUser(resolver,
+                Settings.System.NOTIFICATION_LIGHT_ALWAYS_ALLOWED,
+                mAlwaysAllowedEnabledDefault ? 1 : 0, UserHandle.USER_CURRENT) != 0);
 
             updateNotificationPulse();
 
@@ -3041,7 +3052,7 @@ public class NotificationManagerService extends SystemService {
             enableLed = false;
         } else if (isLedNotificationForcedOn(ledNotification)) {
             enableLed = true;
-        } else if (mInCall || mScreenOn) {
+        } else if (! mAlwaysAllowedEnabledSetting && (mInCall || mScreenOn)) {
             enableLed = false;
         } else {
             enableLed = true;
