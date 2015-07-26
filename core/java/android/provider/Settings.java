7936d1323006429f430787ad9afc125ec64328ea
LorDClockaN <davor@losinj.com>
2015-07-26 23:11:10 +0200

    FWB: statusbar weather font size (1/2)
    Change-Id: I199883313c393027ace02b2fec5dfe8a56f3fde7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3978,6 +3978,12 @@ public final class Settings {
         public static final String STATUS_BAR_WEATHER_COLOR = "status_bar_weather_color";
 
         /**
+         * Statusbar weather font size setting
+         * @hide
+         */
+        public static final String STATUS_BAR_WEATHER_SIZE = "status_bar_weather_size";
+
+        /**
          * Whether to display the trigger region or not
          *
          * Whether to dim the navigation bar icons after inactivity
