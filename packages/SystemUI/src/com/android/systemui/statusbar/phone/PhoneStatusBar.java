omnirom/android_frameworks_base_old

[2/2] base: Add lockscreen rotation as an optional rotation setting
Change-Id: I16606683f1a67abf96d842375d45ebe5ae1bf50b

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -427,8 +427,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     };
 
-    private class NavigationBarObserver extends ContentObserver {
-        NavigationBarObserver(Handler handler) {
+    private class OmniSettingsObserver extends ContentObserver {
+        OmniSettingsObserver(Handler handler) {
             super(handler);
         }
 
@@ -436,6 +436,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_SHOW),
                     false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_ROTATION),
+                    false, this, UserHandle.USER_ALL);
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_ROTATION),
+                    false, this, UserHandle.USER_ALL);
+
             update();
         }
 
@@ -454,9 +461,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     updateNavigationBar();
                 }
             }
+            mStatusBarWindowManager.updateKeyguardScreenRotation();
         }
     }
-    private NavigationBarObserver mNavigationBarObserver = new NavigationBarObserver(mHandler);
+    private OmniSettingsObserver mOmniSettingsObserver = new OmniSettingsObserver(mHandler);
 
     private int mInteractingWindows;
     private boolean mAutohideSuspended;
@@ -633,7 +641,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         addNavigationBar();
         // must be after addNavigationBar
-        mNavigationBarObserver.observe();
+        mOmniSettingsObserver.observe();
 
         // Lastly, call to the icon policy to install/update all the icons.
         mIconPolicy = new PhoneStatusBarPolicy(mContext, mCastController, mHotspotController);
@@ -3277,7 +3285,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateNotifications();
         resetUserSetupObserver();
         setControllerUsers();
-        mNavigationBarObserver.update();
+        mOmniSettingsObserver.update();
     }
 
     private void setControllerUsers() {

mintkat/frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowManager.java
Change-Id: I2ca8280d56349d484f1e7ee0432123b19d6d861a

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -535,6 +535,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.Secure.getUriFor(
                     Settings.Secure.QS_NUM_TILE_COLUMNS), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_ROTATION),false, this, 
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -673,6 +676,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
             // This method reads Settings.Secure.RECENTS_LONG_PRESS_ACTIVITY
             updateCustomRecentsLongPressHandler(false);
+
+            mStatusBarWindowManager.updateKeyguardScreenRotation();
         }
     }
 
@@ -784,6 +789,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             }
         }
     };
+
+    private SettingsObserver mSettingsObserver = new SettingsObserver(mHandler);
 
     private int mInteractingWindows;
     private boolean mAutohideSuspended;
@@ -977,8 +984,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         addNavigationBar();
 
-        SettingsObserver observer = new SettingsObserver(mHandler);
-        observer.observe();
+        mSettingsObserver.observe();
 
         // Lastly, call to the icon policy to install/update all the icons.
         mIconPolicy = new PhoneStatusBarPolicy(mContext, mCastController,
@@ -4100,6 +4106,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (mNavigationBarView != null) {
             mNavigationBarView.updateSettings();
         }
+        mSettingsObserver.update();
     }
 
     private void setControllerUsers() {