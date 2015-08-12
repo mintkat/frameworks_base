17fa257061cc6716462f6d6638e077caa1ad958d
Roman Birg <roman@cyngn.com>
2015-08-12 12:56:31 -0700

    SystemUI: fix empty notification space in shade_locked
    In 8e98622a2c514f8bd11743781d4097e544d2f580, we started
    funneling update notification calls through the handler
    to avoid queueing up hundreds of these calls if a
    notification became spammy and to only execute one update if many were
    queued.
    
    However, some animations and state transitions are expecting the action
    to be done immediately after the call. Facilitate these requests by
    allowing them to bypass the handler if they want.
    
    Ref: CYNGNOS-768
    
    Change-Id: Ice14cdaf93a4d370779b4a3e8996d17cd42fdff8
    Signed-off-by: Roman Birg <roman@cyngn.com>
    (cherry picked from commit f4659b2b91ee6464769f62c771ea7cef7ef99250)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2189,9 +2189,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     }
 
     @Override
-    protected void updateNotifications() {
-        if (!mHandler.hasMessages(MSG_UPDATE_NOTIFICATIONS)) {
-            mHandler.sendEmptyMessage(MSG_UPDATE_NOTIFICATIONS);
+    protected void updateNotifications(boolean immediate) {
+        if (immediate) {
+            mHandler.removeMessages(MSG_UPDATE_NOTIFICATIONS);
+            handleUpdateNotifications();
+        } else {
+            if (!mHandler.hasMessages(MSG_UPDATE_NOTIFICATIONS)) {
+                mHandler.sendEmptyMessage(MSG_UPDATE_NOTIFICATIONS);
+            }
         }
     }
 
@@ -4098,7 +4103,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (MULTIUSER_DEBUG) mNotificationPanelDebugText.setText("USER " + newUserId);
         animateCollapsePanels();
         updatePublicMode();
-        updateNotifications();
+        updateNotifications(true);
         resetUserSetupObserver();
         setControllerUsers();
 
@@ -4861,7 +4866,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         updateDozingState();
         updatePublicMode();
         updateStackScrollerState(goingToFullShade);
-        updateNotifications();
+        updateNotifications(true);
         checkBarModes();
         updateCarrierLabelVisibility(false);
         updateMediaMetaData(false);
