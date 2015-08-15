d83ac7d61bf47a8523fbd8852342b3269f196129
Jorge Ruesga <jorge@ruesga.com>
2015-08-15 13:55:07 +0200

    base: hide qs tiles with sensitive data
    This changes add an option to respect the new sensitive data flag of the QS tiles when
    lockscreen is showing and lockscreen is secure.
    
    Requires: topic:hide-qs-tiles
    
    Change-Id: I92d100cdbf8de47fa6cf574e3a5e5c9da71ae749
    JIRA: CML-140
    JIRA: BACON-3519
    JIRA: CYAN-6720
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>
    Signed-off-by: Adnan Begovic <adnan@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/SyncTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/SyncTile.java
@@ -33,6 +33,10 @@ public class SyncTile extends QSTile<QSTile.BooleanState> {
         super(host);
     }
 
+    public boolean hasSensitiveData() {
+        return true;
+    }
+
     @Override
     protected BooleanState newTileState() {
         return new BooleanState();
