ec922b12e3d26f3ee794bc0575ba30752fb8657c
Jorge Ruesga <jorge@ruesga.com>
2015-09-06 03:23:35 +0200

    base: start nfc service prior to systemui
    Since registration of NfcService as a manager service happens in the first run of the Nfc app,
    and since the SystemUi makes use of this manager service through the NfcTile, NfcManager
    can lead into a null reference for the NfcAdapter because the NfcService isn't registered
    when the SystemUi starts (this condition depends on the device speed to bring up the call
    to the apps).
    This change adds a noop service that allow to be called by SystemServer before start the
    systemui and ensure that NfcService is available before apps start using NfcManager.
    
    Depends on: http://review.cyanogenmod.org/#/c/108684/
    Additional info: http://review.cyanogenmod.org/#/c/108411
    
    Change-Id: Iea84c672762a62946e5708396d8d3b56166eaee8
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1199,6 +1199,14 @@ public final class SystemServer {
                 Slog.i(TAG, "WebViewFactory preparation");
                 WebViewFactory.prepareWebViewInSystemServer();
 
+                // Start Nfc before SystemUi to ensure NfcTile and other apps gets a
+                // valid NfcAdapter from NfcManager
+                try {
+                    startNfcService(context);
+                } catch (Throwable e) {
+                    reportWtf("starting Nfc service", e);
+                }
+
                 try {
                     startSystemUi(context);
                 } catch (Throwable e) {
@@ -1333,4 +1341,12 @@ public final class SystemServer {
         //Slog.d(TAG, "Starting service: " + intent);
         context.startServiceAsUser(intent, UserHandle.OWNER);
     }
+
+    static final void startNfcService(Context context) {
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName("com.android.nfc",
+                    "com.android.nfc.NfcBootstrapService"));
+        context.startServiceAsUser(intent, UserHandle.OWNER);
+    }
+
 }
