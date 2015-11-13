e2194b0755db17fe4f6da45d0e74366f46588d6c
bigrushdog <randall.rushing@gmail.com>
2015-11-13 00:50:14 -0800

    Statusbar: Gracefully adapt 3Minit Battery hook for AOSP
    3Minit Battery is a free or paid third party statusbar
    battery icon solution. This commit builds upon the
    initial SystemUI hook source release by Gary Harrington.
    
    We are able to significantly optimize the original code
    because we are not constrained by the "one-size-fits-all"
    conditions the original code must meet. At the same time,
    we fully maintain the integrity of the UX intended by
    the original code.
    
    Of note is the introduction of a controller class. This
    class takes over settings and state management as well
    as handles receiver events. The controller class also
    monitors package changes and updates feature visibility
    accordingly if a needed resource is added or removed.
    
    Upon initialization, if no feature resources are
    detected, all resources are freed.
    
    AICPfy:
    Add needed classes from DU
    
    Change-Id: I0254bfa0becfbf6e83e9d0547abda8a77047ab1e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -131,6 +131,7 @@ import com.android.internal.statusbar.NotificationVisibility;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.util.benzo.Helpers;
+import com.android.internal.util.benzo.DUPackageMonitor;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
@@ -188,6 +189,7 @@ import com.android.systemui.statusbar.policy.KeyguardMonitor;
 import com.android.systemui.statusbar.policy.KeyguardUserSwitcher;
 import com.android.systemui.statusbar.policy.LocationControllerImpl;
 import com.android.systemui.statusbar.policy.MinitBattery;
+import com.android.systemui.statusbar.policy.MinitBatteryController;
 import com.android.systemui.statusbar.policy.NetworkControllerImpl;
 import com.android.systemui.statusbar.policy.NextAlarmController;
 import com.android.systemui.statusbar.policy.PreviewInflater;
@@ -336,6 +338,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     BrightnessMirrorController mBrightnessMirrorController;
     AccessibilityController mAccessibilityController;
     FingerprintUnlockController mFingerprintUnlockController;
+    MinitBatteryController mMinitBatteryController;
     WeatherControllerImpl mWeatherController;
     StatusBarBgAnimationController mStatusBarBgAnimationController;
     SuControllerImpl mSuController;
@@ -473,6 +476,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     private int mBlurRadius;
     private Bitmap mBlurredImage = null;
+
+    private DUPackageMonitor mPackageMonitor;
 
     Runnable mLongPressBrightnessChange = new Runnable() {
         @Override
@@ -1042,6 +1047,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mStatusBarView = (PhoneStatusBarView) mStatusBarWindow.findViewById(R.id.status_bar);
         mStatusBarView.setBar(this);
 
+        mPackageMonitor = new DUPackageMonitor();
+        mPackageMonitor.register(mContext, mHandler);
+
         PanelHolder holder = (PanelHolder) mStatusBarWindow.findViewById(R.id.panel_holder);
         mStatusBarView.setPanelHolder(holder);
 
@@ -1175,6 +1183,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // set the inital view visibility
         setAreThereNotifications();
+
+        mMinitBatteryController = new MinitBatteryController(mContext, mStatusBarView, mHeader, mKeyguardStatusBar);
+        mPackageMonitor.addListener(mMinitBatteryController);
 
         mIconController = new StatusBarIconController(
                 mContext, mStatusBarView, mKeyguardStatusBar, this);
@@ -1330,12 +1341,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mKeyguardStatusBar.setBatteryController(mBatteryController);
         mHeader.setNextAlarmController(mNextAlarmController);
         mHeader.setWeatherController(mWeatherController);
-
-        MinitBattery mb = (MinitBattery) mStatusBarView.findViewById(R.id.minitBattery);
-        if (!mb.isSetup()) {
-            BatteryMeterView bmv = (BatteryMeterView) mStatusBarView.findViewById(R.id.battery);
-            bmv.setVisibility(View.VISIBLE);
-        }
 
         PowerManager pm = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
         mBroadcastReceiver.onReceive(mContext,
@@ -4200,6 +4205,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
         mContext.unregisterReceiver(mBroadcastReceiver);
         mContext.unregisterReceiver(mDemoReceiver);
+        mPackageMonitor.removeListener(mMinitBatteryController);
+        mPackageMonitor.unregister();
         mAssistManager.destroy();
 
         final SignalClusterView signalCluster =
