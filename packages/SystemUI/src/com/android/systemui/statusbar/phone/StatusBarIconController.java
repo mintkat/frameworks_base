leadpoizon/android_frameworks_base

Clock: fix left and center clock not using dark mode

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -435,6 +435,8 @@ public class StatusBarIconController implements Tunable {
         mMoreIcon.setImageTintList(ColorStateList.valueOf(mIconTint));
         mBatteryMeterView.setDarkIntensity(mDarkIntensity);
         mClock.setTextColor(mIconTint);
+        mCclock.setTextColor(mIconTint);
+        mLeftClock.setTextColor(mIconTint);
         mNetworkTraffic.setDarkIntensity(mDarkIntensity);
         applyNotificationIconsTint();
     }

mintkat/frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -441,6 +441,8 @@ public class StatusBarIconController implements Tunable {
         mBatteryLevelTextView.setTextColor(mIconTint);
         mBatteryMeterView.setDarkIntensity(mDarkIntensity);
         mClock.setTextColor(mIconTint);
+        mCenterClock.setTextColor(mIconTint);
+        mLeftClock.setTextColor(mIconTint);
         applyNotificationIconsTint();
     }
 