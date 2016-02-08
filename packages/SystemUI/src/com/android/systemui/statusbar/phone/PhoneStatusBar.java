54bf938250f3c4adfa7c2f20b6a541836cd9e0e9
LorDClockaN <davor@losinj.com>
2016-02-08 18:40:57 +0100

    FWB: App picker navbar button
    Found some tutorial on line on how to make app picker
    app dialog, so I've adopted it to the rom
    
    Change-Id: I8165f6ad4f232921e16cc164923fa393fc731e40

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1571,6 +1571,13 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     };
 
+    //App picker
+    private final View.OnClickListener mAppPickerClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            appPicker();
+        }
+    };
+
     private void awakenDreams() {
         if (mDreamManager != null) {
             try {
@@ -1747,13 +1754,19 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
+    private void appPicker() {
+        Intent intent = new Intent(".benzo.apppicker.AppPickerActivity");
+        startActivity(intent, true);
+    }
+
     private void prepareNavigationBarView() {
         mNavigationBarView.reorient();
 
         mNavigationBarView.setListeners(mRecentsClickListener, mRecentsPreloadOnTouchListener,
                 mLongPressBackRecentsListener, mHomeActionListener, mLongPressHomeListener,
                 mNotificationsClickListener, mNotificationsLongListener, mTorchClickListener,
-                mCameraClickListener, mScreenShotClickListener, mImmersiveClickListener);
+                mCameraClickListener, mScreenShotClickListener, mImmersiveClickListener,
+                mAppPickerClickListener);
         mAssistManager.onConfigurationChanged();
     }
 
