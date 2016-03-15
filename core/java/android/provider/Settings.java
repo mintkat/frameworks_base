09fc10ad7854ae7a36b624552ac6f15e8fcab017
rogersb11 <brettrogers11@gmail.com>
2016-03-15 00:43:15 -0400

    Dashboard Text Size 2/2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5220,6 +5220,18 @@ public final class Settings {
         public static final String SCREENSHOT_DELAY = "screenshot_delay";
 
         /**
+         * Dashboard title text size
+         * @hide
+         */
+        public static final String SETTINGS_TITLE_TEXT_SIZE = "settings_title_text_size";
+
+        /**
+         * Dashboard category text size
+         * @hide
+         */
+        public static final String SETTINGS_CATEGORY_TEXT_SIZE = "settings_category_text_size";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
